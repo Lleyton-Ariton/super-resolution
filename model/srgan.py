be5b22c016a57628cafb9835d2d8a32ba3e65c22
Lleyton-Ariton <lleytonariton@MacBookLleyton.local>
2020-12-30 17:22:22 -0500

    added discriminator network

--- model/srgan.py
+++ model/srgan.py
@@ -1,31 +1,9 @@
 import torch
-import torch.nn as nn
-
 import numpy as np
 
+import torch.nn as nn
 
-class ConvBlock(nn.Module):
-
-    def __init__(self, in_channels: int, out_channels: int, kernel_size: int,
-                 stride: int=1, padding: int=1, bias: bool=False):
-        super().__init__()
-        self.params = {
-            'in_channels': in_channels,
-            'out_channels': out_channels,
-            'kernel_size': kernel_size,
-            'stride': stride,
-            'padding': padding,
-            'bias': bias
-        }
-
-        self.conv_block = nn.Sequential(
-            nn.Conv2d(**self.params),
-            nn.BatchNorm2d(self.params['out_channels']),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True)
-        )
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.conv_block(x)
+from torchvision.models import vgg19_bn
 
 
 class UpsamplingBlock(nn.Module):
@@ -117,8 +95,79 @@ class GeneratorNetwork(nn.Module):
         return x
 
 
+class ConvBlock(nn.Module):
+
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int,
+                 stride: int=1, padding: int=1, bias: bool=False):
+        super().__init__()
+        self.params = {
+            'in_channels': in_channels,
+            'out_channels': out_channels,
+            'kernel_size': kernel_size,
+            'stride': stride,
+            'padding': padding,
+            'bias': bias
+        }
+
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(**self.params),
+            nn.BatchNorm2d(self.params['out_channels']),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.conv_block(x)
+
+
 class DiscriminatorNetwork(nn.Module):
 
-    def __init__(self):
+    def __init__(self, original: bool=False, pretrained: bool=True):
         super().__init__()
-        pass  # Not Finished
+        self.original = original
+        self.pretrained = False
+
+        avgpool = nn.AdaptiveAvgPool2d((14, 14))
+        flatten = nn.Flatten()
+        classifier = nn.Sequential(
+                nn.Linear(512 * 14 * 14, 1024),
+                nn.LeakyReLU(negative_slope=0.2, inplace=True),
+                nn.Linear(1024, 1),
+                nn.Sigmoid()
+            )
+
+        if self.original:
+            input_layer = nn.Sequential(
+                nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                nn.LeakyReLU(negative_slope=0.2, inplace=True)
+            )
+
+            body = []
+            for i in range(6, 9):
+                in_channels, out_channels = pow(2, i), pow(2, i + 1)
+
+                body.extend([
+                    ConvBlock(in_channels, in_channels, kernel_size=3, stride=2),
+                    ConvBlock(in_channels, out_channels, kernel_size=3)
+                ])
+            body.append(
+                ConvBlock(512, 512, kernel_size=3, stride=2)
+            )
+
+            self.discriminator = nn.Sequential(
+                input_layer,
+                *body,
+                avgpool,
+                flatten,
+                classifier
+            )
+
+        else:
+            self.pretrained = pretrained
+
+            self.discriminator = vgg19_bn(self.pretrained)
+
+            self.discriminator.avgpool = avgpool
+            self.discriminator.classifier = classifier
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.discriminator(x)
